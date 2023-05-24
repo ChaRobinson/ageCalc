13e6916ea5bed5122a947ccec17501b65aea2452
lilbigangel86 <lilbigangel86@yahoo.com>
2023-05-24 18:23:33 -0400

    update 3

---
+++ ageCalc/script.js
@@ -0,0 +1,114 @@
+// we select the output elements
+const output_year = document.querySelector('.output-year');
+const output_month = document.querySelector('.output-month');
+const output_day = document.querySelector('.output-day');
+const submit_btn = document.querySelector('.submit-btn');
+//Input Elements
+
+const input_year= document.querySelector('#year');
+const input_day = document.querySelector('#day');
+const input_month = document.querySelector('#month');
+//const submit_btn = document.querySelector('.submit-btn');
+//ERROR ELEMENTS
+const error_day = document.querySelector('.error-day');
+const error_month = document.querySelector('.error-month');
+const error_year = document.querySelector('.error-year');
+submit_btn.addEventListener('click', CalculateDate)
+input_day.addEventListener('input', e =>{
+    if (+input_day.value > 31){
+        error_day.textContent = 'Must be a valid date';
+        isValid = false;
+        return;
+    }
+    else {
+        isValid = true;
+        error_day.textContent = "";
+    }
+    if (+input_day.value === 0){
+        isValid = false
+        error_day.textContent = 'This field is required';
+        isValid = false;
+        return;
+    } else {
+        error_day.textContent = "";
+    }
+});
+
+input_month.addEventListener('input', e =>{
+    if (+input_month.value > 12){
+        error_month.textContent = 'Must be a valid date';
+        isValid = false;
+        return;
+    }
+    else {
+        isValid = true;
+        error_month.textContent = "";
+    }
+    if (+input_month.value === 0){
+        isValid = false
+        input_month.textContent = 'This field is required';
+        isValid = false;
+        return;
+    } else {
+        input_month.textContent = "";
+    }
+});
+
+input_year.addEventListener('input', e =>{
+    if (+input_year.value > 2023){
+        error_year.textContent = 'Must be a valid date';
+        isValid = false;
+        return;
+    }
+    else {
+        isValid = true;
+        error_year.textContent = "";
+    }
+    if (+input_year.value === 0){
+        isValid = false
+        input_year.textContent = 'This field is required';
+        isValid = false;
+        return;
+    } else {
+        input_year.textContent = "";
+    }
+});
+
+input_month.addEventListener('input', e =>{
+    if (+input_month.value > 31){
+        error_month.textContent = 'Must be a valid date';
+        isValid = false;
+        return;
+    }
+    else {
+        isValid = true;
+        error_month.textContent = "";
+    }
+    if (+input_month.value === 0){
+        isValid = false
+        input_month.textContent = 'This field is required';
+        isValid = false;
+        return;
+    } else {
+        input_month.textContent = "";
+    }
+});
+
+function CalculateDate(){
+    if(isValid) {
+let birthday = `${input_month.value}/${input_day.value}/${input_year.value}`;
+console.log(birthday);
+letBirthdayObj = new Date(birthday);
+let ageDiffMill = Date.now() - birthdayObj;
+let ageDate = new Date(ageDiffMill);
+let ageYear = ageDate.getUTCFullYear() - 1970
+let ageMonth = ageDate.getUTCMonth();
+let ageDay = ageDate.getUTCDay();
+//DISPLAY EVERYTHING
+output_day.textContent = ageDay;
+output_month.textContent = ageMonth;
+output_year.textContent = ageYears;
+    } else {
+        alert("error");
+    }
+}
